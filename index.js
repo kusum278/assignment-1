c41d2b13ccce220f41edc6401a90a94b83bdd0e3
kusum278 <85170195+kusum278@users.noreply.github.com>
2021-08-03 18:08:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Day 1</title>
+  </head>
+  <body>
+    <h1>Day 1 Assignment</h1>
+    <script>
+      let a = 3;
+      let b = 2;
+      console.log(`Add : ${a + b}`);
+      console.log(`Subtract : ${a - b}`);
+      console.log(`Multiply : ${a * b}`);
+      console.log(`Divide : ${a / b}`);
+    </script>
+  </body>
+</html>
